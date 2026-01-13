84e2cb876050cf32da54230c690470d94fef3462
mf010 <mohammedfurrara1@gmail.com>
2026-01-13 03:23:39 +0200

    fix roat issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/react/', // ✅ REQUIRED
+  base: '/public/react/', // ✅ REQUIRED
   plugins: [react()],
   resolve: {
     alias: {
